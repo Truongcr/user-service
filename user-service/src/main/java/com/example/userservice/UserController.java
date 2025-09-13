460d3fe128376aed8e179ac51a8c5055f8301daa
Truongcr <truongcr1609@gmail.com>
2025-09-13 15:50:23 +0700

    1

--- user-service/src/main/java/com/example/userservice/UserController.java
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
         System.out.println("UserController initialized with repo: " + repo);
         System.out.println("UserController initialized with repo: " + repo);
         System.out.println("UserController initialized with repo: " + repo);
-        System.out.println("UserController initialized with repo: " + repo);
+        System.out.println("UserController initialized with repo1: " + repo);
         this.repo = repo;
     }
 
